f97a975fa20db148a4b7f10960022163a84629ff
hawi-claire <7hawi7@gmail.com>
2024-04-23 06:20:36 -0700

    [Fix] Code

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,10 +1,6 @@
 # Install flask from pip3
-class { 'python3': }
 
 package { 'flask':
-  ensure   => present,
-  provider => 'pip3',
-  version: '2.1.0'
-  require => Class['python3'],
+  ensure    =>   2.1.0
+  provider  =>  'pip3
 }
-
