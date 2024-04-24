0d55394322f4d2b0025fde02bbbb05470d83c1e6
hawi-claire <7hawi7@gmail.com>
2024-04-24 14:04:07 -0700

    [Fix] Errors

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 # This code will install the package puppet-lint
-class { 'python3': }
+class { 'python3:' }
 
 package { 'flask':
   ensure => present,
   provider => 'pip3',
-  version: '2.1.0'
+  version => '2.1.0',
   require => Class['python3'],
 }
