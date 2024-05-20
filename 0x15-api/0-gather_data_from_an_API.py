f71c610732b98d40595d9e1bda42d63026b05758
hawi-claire <7hawi7@gmail.com>
2024-05-20 14:28:18 -0700

    [Fix] 0. Gather data from an API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 """ Returns employee information using REST api """
 
 import re
@@ -12,9 +11,9 @@ if __name__ == '__main__':
     if len(sys.argv) > 1:
         if re.fullmatch(r'\d+', sys.argv[1]):
             id = int(sys.argv[1])
-            emp_req = requests.get('{}/users/{}'.format(REST_API, id)).json()
+            req = requests.get('{}/users/{}'.format(REST_API, id)).json()
             task_req = requests.get('{}/todos'.format(REST_API)).json()
-            emp_name = emp_req.get('name')
+            emp_name = req.get('name')
             tasks = list(filter(lambda x: x.get('userId') == id, task_req))
             completed_tasks = list(filter(lambda x: x.get('completed'), tasks))
             print(
