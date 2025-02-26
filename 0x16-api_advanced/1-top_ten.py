c1f7f97947e85d08755e89b676060d1a573747ef
hawi-claire <7hawi7@gmail.com>
2025-02-26 15:13:00 -0800

    [Update: Changes]

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,33 +1,44 @@
 #!/usr/bin/python3
 
-import requests
+"""
+Write a function that queries the Reddit API and prints
+the titles of the first 10 hot posts listed for a given subreddit.
+
+Requirements:
+    - Prototype: def top_ten(subreddit)
+    - If not a valid subreddit, print None.
+    - Note: Invalid subreddits may return a redirect to search results.
+      Ensure that you are not following redirects.
+"""
+
+
+from requests import get
+
 
 def top_ten(subreddit):
     """
-    Queries the Reddit API and prints the titles of the first 10 hot posts
-    listed for a given subreddit.
+    Queries the Reddit API and print the titles
+    of the first 10 hot posts listed for a given subreddit
 
     Args:
-        subreddit (str): The name of the subreddit.
+        subreddit (str): The name of the subreddit
+
+    Return:
+        Void
     """
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
-    headers = {"User-Agent": "MyRedditBot/1.0"}
 
-    try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
-        response.raise_for_status()
+    if subreddit is None:
+        return print("None")
 
-        if response.status_code == 200:
-            data = response.json()
-            posts = data["data"]["children"]
-            for post in posts:
-                print(post["data"]["title"])
-        elif response.status_code == 404:
-            print(None)
-        else:
-            print(None)
+    api_endpoint = 'https://reddit.com/r/{}/hot.json'.format(
+        subreddit)
 
-    except requests.exceptions.RequestException:
-        print(None)
-    except (KeyError, ValueError):
-        print(None)
+    response = get(api_endpoint, params={'limit': 10})
+
+    if response.ok is False:
+        return print("None")
+
+    posts = response.json()
+
+    for post in posts.get('data').get('children'):
+        print(post.get('data').get('title'))
