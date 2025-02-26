1172aab4b1e28e2f488a9c463aceacd0a4c7a5d9
hawi-claire <7hawi7@gmail.com>
2025-02-26 00:40:44 -0800

    [fix: Remove except and try]

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,31 +5,15 @@ Script that queries subscribers on a given Reddit subreddit.
 
 import requests
 
+
 def number_of_subscribers(subreddit):
-    """
-    Queries the Reddit API and returns the number of subscribers for a given subreddit.
-
-    Args:
-    subreddit (str): The name of the subreddit.
-
-    Returns:
-    int: The number of subscribers, or 0 if the subreddit is invalid.
-    """
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {"User-Agent": "MyRedditBot/1.0"}
+    """Return the total number of subscribers on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {"User-Agent": "Mozilla/5.0"}
     response = requests.get(url, headers=headers, allow_redirects=False)
-    response.raise_for_status()
     if response.status_code == 200:
         data = response.json()
-        subscribers = data["data"]["subscribers"]
+        subscribers = data['data']['subscribers']
         return subscribers
-    elif response.status_code == 404:
-        return 0
     else:
-        return 0
-    except requests.exceptions.RequestException as e:
-        return 0
-    except KeyError:
-        return 0
-    except ValueError:
         return 0
